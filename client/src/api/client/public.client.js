93e355decd0e9800ee4014757964123c394ad0c3
techdhasarthan <dhasarathandharmaraj@gmail.com>
2023-04-06 11:22:30 +0530

    changw
    changw

--- client/src/api/client/public.client.js
+++ client/src/api/client/public.client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseURL = "mongodb://127.0.0.1:27017/";
+const baseURL = "https://movie-info-sandy.vercel.app/api/v1";
 
 const publicClient = axios.create({
   baseURL,
